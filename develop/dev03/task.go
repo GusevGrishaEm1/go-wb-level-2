de3c93b0952abda95c8b0b13daa3bb3de244bc8c
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-14 00:41:23 +0400

    dev03 completed

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
@@ -17,7 +25,7 @@ package main
 
 Поддержать ключи
 
--M — сортировать по названию месяца
+-m — сортировать по названию месяца
 -b — игнорировать хвостовые пробелы
 -c — проверять отсортированы ли данные
 -h — сортировать по числовому значению с учётом суффиксов
@@ -26,5 +34,84 @@ package main
 */
 
 func main() {
+	config := ParseFlags()
+	result, err := Sort("test1.txt", config)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		return
+	}
+	fmt.Print(result)
+}
 
+type Config struct {
+	Column           string
+	SortNumerical    bool
+	ReverseSort      bool
+	UniqueValues     bool
+	SortByMonth      bool
+	IgnoreTrailing   bool
+	CheckSorted      bool
+	SortSuffixValues bool
+}
+
+func ParseFlags() Config {
+	config := Config{}
+
+	flag.StringVar(&config.Column, "k", "", "Specify the column to sort by")
+	flag.BoolVar(&config.SortNumerical, "n", false, "Sort by numerical value")
+	flag.BoolVar(&config.ReverseSort, "r", false, "Sort in reverse order")
+	flag.BoolVar(&config.UniqueValues, "u", false, "Do not display duplicate lines")
+	flag.BoolVar(&config.SortByMonth, "M", false, "Sort by month name")
+	flag.BoolVar(&config.IgnoreTrailing, "b", false, "Ignore trailing whitespace")
+	flag.BoolVar(&config.CheckSorted, "c", false, "Check if the data is sorted")
+	flag.BoolVar(&config.SortSuffixValues, "h", false, "Sort by numerical value with suffixes")
+
+	flag.Parse()
+
+	return config
+}
+
+func Sort(input string, c Config) ([]string, error) {
+	cmd := exec.Command("sort")
+
+	if c.Column != "" {
+		cmd.Args = append(cmd.Args, "-k"+c.Column)
+	}
+
+	if c.SortNumerical {
+		cmd.Args = append(cmd.Args, "-n")
+	}
+
+	if c.ReverseSort {
+		cmd.Args = append(cmd.Args, "-r")
+	}
+
+	if c.UniqueValues {
+		cmd.Args = append(cmd.Args, "-u")
+	}
+
+	if c.SortByMonth {
+		cmd.Args = append(cmd.Args, "-M")
+	}
+
+	if c.IgnoreTrailing {
+		cmd.Args = append(cmd.Args, "-b")
+	}
+
+	if c.CheckSorted {
+		cmd.Args = append(cmd.Args, "-c")
+	}
+
+	if c.SortSuffixValues {
+		cmd.Args = append(cmd.Args, "-h")
+	}
+
+	cmd.Args = append(cmd.Args, input)
+
+	out, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(string(out), "\r\n"), nil
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortWithColumn(t *testing.T) {
+	config := Config{Column: "2"}
+
+	expected := []string{"d a", "c b", "b c", "a d"}
+
+	output, err := Sort("test1.txt", config)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !reflect.DeepEqual(output, expected) {
+		t.Errorf("Expected: %v, got: %v", expected, output)
+	}
+}
+
+// func TestSortNumerical(t *testing.T) {
+// 	input := []string{"3", "1", "10", "2"}
+// 	config := Config{SortNumerical: true}
+// 	expected := "1n2n3n10n"
+// 	result, err := Sort(input, config)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	if result != expected {
+// 		t.Errorf("Sort didn't produce the expected result. Got: %s, Expected: %s", result, expected)
+// 	}
+// }
