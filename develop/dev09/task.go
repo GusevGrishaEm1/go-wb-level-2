a3147fd6114557bfd8802e916b02bb13f6266aa9
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-14 20:08:55 +0400

    dev09 completed

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -8,6 +8,45 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
 
+func downloadPage(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	filename := "index.html"
+	if strings.HasSuffix(url, "/") {
+		filename += "index.html"
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	url := "https://www.example.com"
+	err := downloadPage(url)
+	if err != nil {
+		fmt.Println("Error downloading page:", err)
+		return
+	}
+	fmt.Println("Page downloaded successfully!")
 }
--- develop/dev09/task_test.go
+++ develop/dev09/task_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestDownloadPage(t *testing.T) {
+	url := "https://www.example.com"
+	err := downloadPage(url)
+	if err != nil {
+		t.Errorf("Error downloading page: %v", err)
+	}
+
+	// Проверяем, что файл был создан
+	if _, err := os.Stat("index.html"); os.IsNotExist(err) {
+		t.Errorf("Expected file index.html to be created, but it wasn't")
+	}
+
+	// Проверяем содержимое файла
+	content, err := os.ReadFile("index.html")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	expectedContent := []byte("Example Domain")
+	if !bytes.Contains(content, expectedContent) {
+		t.Errorf("Expected content not found in the downloaded page")
+	}
+
+	// Чистим за собой после теста
+	os.Remove("index.html")
+}
