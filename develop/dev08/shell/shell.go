0a8ca26b7b77263e44e9ee1e7375f88b7d4b27ac
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-18 00:43:29 +0400

    fix

--- develop/dev08/shell/shell.go
+++ develop/dev08/shell/shell.go
@@ -31,76 +31,78 @@ func main() {
 			break
 		}
 
-		args := strings.Fields(input)
+		commands := strings.Split(input, "|")
 
-		switch args[0] {
-		case "cd":
-			if len(args) > 1 {
-				if err := os.Chdir(args[1]); err != nil {
-					fmt.Println("Error changing directory:", err)
+		for _, command := range commands {
+			args := strings.Fields(command)
+
+			switch args[0] {
+			case "cd":
+				if len(args) > 1 {
+					if err := os.Chdir(args[1]); err != nil {
+						fmt.Println("Error changing directory:", err)
+					}
+				} else {
+					fmt.Println("Usage: cd <directory>")
 				}
-			} else {
-				fmt.Println("Usage: cd <directory>")
-			}
-		case "pwd":
-			pwd, err := os.Getwd()
-			if err != nil {
-				fmt.Println("Error getting current directory:", err)
-			} else {
-				fmt.Println(pwd)
-			}
-		case "echo":
-			if len(args) > 1 {
-				fmt.Println(strings.Join(args[1:], " "))
-			} else {
-				fmt.Println("Usage: echo <text>")
-			}
-		case "kill":
-			if len(args) > 1 {
-				var pid int
-				pid, err = strconv.Atoi(args[1])
+			case "pwd":
+				pwd, err := os.Getwd()
 				if err != nil {
-					fmt.Println("Error converting PID:", err)
-					return
+					fmt.Println("Error getting current directory:", err)
+				} else {
+					fmt.Println(pwd)
 				}
-				process, err := os.FindProcess(pid)
+			case "echo":
+				if len(args) > 1 {
+					fmt.Println(strings.Join(args[1:], " "))
+				} else {
+					fmt.Println("Usage: echo <text>")
+				}
+			case "kill":
+				if len(args) > 1 {
+					var pid int
+					pid, err = strconv.Atoi(args[1])
+					if err != nil {
+						fmt.Println("Error converting PID:", err)
+						return
+					}
+					process, err := os.FindProcess(pid)
+					if err != nil {
+						fmt.Println("Error finding process:", err)
+						return
+					}
+					err = process.Signal(syscall.SIGKILL)
+					if err != nil {
+						fmt.Println("Error send signal:", err)
+						return
+					}
+					fmt.Println("Process with pid ", pid, "killed")
+				} else {
+					fmt.Println("Usage: kill <pid>")
+				}
+			case "ps":
+				var cmd *exec.Cmd
+				if runtime.GOOS == "windows" {
+					cmd = exec.Command("tasklist")
+				} else {
+					cmd = exec.Command("ps", "aux")
+				}
+				output, err := cmd.Output()
 				if err != nil {
-					fmt.Println("Error finding process:", err)
-					return
+					fmt.Println("error: ", err)
 				}
-				err = process.Signal(syscall.SIGKILL)
-				if err != nil {
-					fmt.Println("Error send signal:", err)
-					return
-				}
-				fmt.Println("Process with pid ", pid, "killed")
-			} else {
-				fmt.Println("Usage: kill <pid>")
+				fmt.Println(string(output))
+			case "fork":
+				// id, _, errno := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+				// if errno != 0 {
+				// 	fmt.Println("Fork call error:", err)
+				// }
+				// if id == 0 {
+				// 	fmt.Print("Child process: ", id)
+				// } else {
+				// 	fmt.Print("Parent process: ", id)
+				// }
 			}
-		case "ps":
-			var cmd *exec.Cmd
-			if runtime.GOOS == "windows" {
-				cmd = exec.Command("tasklist")
-			} else {
-				cmd = exec.Command("ps", "aux")
-			}
-			output, err := cmd.Output()
-			if err != nil {
-				fmt.Println("error: ", err)
-			}
-			fmt.Println(string(output))
-		default:
-			pid, err := syscall.Fork()
-			if err != nil {
-				fmt.Println("Error:", err)
-			}
-			if pid == 0 {
-				err = syscall.Exec("/bin/ls", []string{"ls"}, nil)
-				if err != nil {
-					fmt.Println("Error:", err)
-				}
-			}
-			fmt.Println("PID:", pid)
 		}
 	}
 }
