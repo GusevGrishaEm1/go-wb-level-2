51293607d47a1a05266ea93926e492882d151c75
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-18 17:21:50 +0400

    fix

--- develop/dev08/shell/shell.go
+++ develop/dev08/shell/shell.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
-	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
@@ -92,12 +91,7 @@ func executeCommand(command, output string) (string, error) {
 			return "", fmt.Errorf("Usage: kill <pid>")
 		}
 	case "ps":
-		var cmd *exec.Cmd
-		if runtime.GOOS == "windows" {
-			cmd = exec.Command("tasklist")
-		} else {
-			cmd = exec.Command("ps", "aux")
-		}
+		cmd := exec.Command("ps", "aux")
 		outputBytes, err := cmd.Output()
 		if err != nil {
 			return "", fmt.Errorf("error: %w", err)
