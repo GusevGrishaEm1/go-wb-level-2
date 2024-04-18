fe070dfd237731017c3cb0aa49d2dfe637431595
GusevGrishaEm1 <sibrim1212@gmail.com>
2024-04-18 16:51:26 +0400

    iter1

--- develop/dev08/shell/shell.go
+++ develop/dev08/shell/shell.go
@@ -12,6 +12,12 @@ import (
 	"syscall"
 )
 
+type command struct {
+	command string
+	input   string
+	output  string
+}
+
 func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
@@ -33,76 +39,83 @@ func main() {
 
 		commands := strings.Split(input, "|")
 
-		for _, command := range commands {
-			args := strings.Fields(command)
+		output := ""
 
-			switch args[0] {
-			case "cd":
-				if len(args) > 1 {
-					if err := os.Chdir(args[1]); err != nil {
-						fmt.Println("Error changing directory:", err)
-					}
-				} else {
-					fmt.Println("Usage: cd <directory>")
-				}
-			case "pwd":
-				pwd, err := os.Getwd()
-				if err != nil {
-					fmt.Println("Error getting current directory:", err)
-				} else {
-					fmt.Println(pwd)
-				}
-			case "echo":
-				if len(args) > 1 {
-					fmt.Println(strings.Join(args[1:], " "))
-				} else {
-					fmt.Println("Usage: echo <text>")
-				}
-			case "kill":
-				if len(args) > 1 {
-					var pid int
-					pid, err = strconv.Atoi(args[1])
-					if err != nil {
-						fmt.Println("Error converting PID:", err)
-						return
-					}
-					process, err := os.FindProcess(pid)
-					if err != nil {
-						fmt.Println("Error finding process:", err)
-						return
-					}
-					err = process.Signal(syscall.SIGKILL)
-					if err != nil {
-						fmt.Println("Error send signal:", err)
-						return
-					}
-					fmt.Println("Process with pid ", pid, "killed")
-				} else {
-					fmt.Println("Usage: kill <pid>")
-				}
-			case "ps":
-				var cmd *exec.Cmd
-				if runtime.GOOS == "windows" {
-					cmd = exec.Command("tasklist")
-				} else {
-					cmd = exec.Command("ps", "aux")
-				}
-				output, err := cmd.Output()
-				if err != nil {
-					fmt.Println("error: ", err)
-				}
-				fmt.Println(string(output))
-			case "fork":
-				id, _, errno := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
-				if errno != 0 {
-					fmt.Println("Fork call error:", err)
-				}
-				if id == 0 {
-					fmt.Print("Child process: ", id)
-				} else {
-					fmt.Print("Parent process: ", id)
-				}
+		for _, command := range commands {
+			output, err = executeCommand(command, output)
+			if err != nil {
+				fmt.Println(err)
+				return
 			}
 		}
+
+		fmt.Println(output)
 	}
+}
+
+func executeCommand(command, output string) (string, error) {
+	args := strings.Fields(command)
+	var err error
+
+	switch args[0] {
+	case "cd":
+		if len(args) > 1 {
+			if err = os.Chdir(args[1]); err != nil {
+				return "", fmt.Errorf("Error changing directory: %w", err)
+			}
+		} else {
+			return "", fmt.Errorf("Usage: cd <directory>")
+		}
+	case "pwd":
+		pwd, err := os.Getwd()
+		if err != nil {
+			return "", fmt.Errorf("Error getting current directory: %w", err)
+		}
+		output = pwd
+	case "echo":
+		if len(args) > 1 {
+			output = strings.Join(args[1:], " ")
+		} else {
+			return "", fmt.Errorf("Usage: echo <text>")
+		}
+	case "kill":
+		if len(args) > 1 {
+			var pid int
+			pid, err = strconv.Atoi(args[1])
+			if err != nil {
+				return "", fmt.Errorf("Error converting PID: %w", err)
+			}
+			process, err := os.FindProcess(pid)
+			if err != nil {
+				return "", fmt.Errorf("Error finding process: %w", err)
+			}
+			err = process.Signal(syscall.SIGKILL)
+			if err != nil {
+				return "", fmt.Errorf("Error sending signal: %w", err)
+			}
+			output = fmt.Sprintf("Process with pid %d killed", pid)
+		} else {
+			return "", fmt.Errorf("Usage: kill <pid>")
+		}
+	case "ps":
+		var cmd *exec.Cmd
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command("tasklist")
+		} else {
+			cmd = exec.Command("ps", "aux")
+		}
+		outputBytes, err := cmd.Output()
+		if err != nil {
+			return "", fmt.Errorf("error: %w", err)
+		}
+		output = string(outputBytes)
+	case "fork":
+		fmt.Print("fork")
+	case "exec":
+		fmt.Print("exec")
+	default:
+		return "", fmt.Errorf("Unknown command: %s", args[0])
+	}
+
+	return output, nil
 }
